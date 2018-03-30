5c10ba38a7e374ec19207dc1eb771715874fdc2d
Hadron Megantara <hadron@Macs-MacBook-Pro.local>
2018-03-31 04:29:36 +0700

    Produc Detail footer

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -17,49 +17,51 @@
 					</div>
 					<div class="col-md-2 col-sm-4">
 						<div class="footer-widget">
-							<h3 class="footer-title">My account</h3>
+							<h3 class="footer-title">Telusuri Kami</h3>
 							<ul class="block-content">
-								<li><a href="#">My orders</a></li>
-								<li><a href="#">My credit slips</a></li>
-								<li><a href="#">Sitemap</a></li>
-								<li><a href="#">My addresses</a></li>
-								<li><a href="#">My personal info</a></li>
+								<li><a href="#">Tentang Kangkoding</a></li>
+								<li><a href="#">Press</a></li>
+								<li><a href="#">Kebijakan</a></li>
+								<li><a href="#">Syarat & Ketentuan</a></li>
+								<li><a href="#">Kontak Kami</a></li>
 							</ul>
 						</div>
 					</div>
 					<div class="col-md-2 col-sm-4">
 						<div class="footer-widget">
-							<h3 class="footer-title">Information</h3>
+							<h3 class="footer-title">Bantuan</h3>
 							<ul class="block-content">
-								<li><a href="#">Contact us</a></li>
-								<li><a href="#">Discount</a></li>
-								<li><a href="#">Site map</a></li>
-								<li><a href="#">About us</a></li>
-								<li><a href="#">Custom service</a></li>
+								<li><a href="#">Panduan Pembayaran</a></li>
+								<li><a href="#">Pengembalian & Penukaran</a></li>
+								<li><a href="#">Pengiriman</a></li>
+								<li><a href="#">FAQ</a></li>
+								<li><a href="#">Size Guide</a></li>
+								<li><a href="#">Panduan Material</a></li>
 							</ul>
 						</div>
 					</div>
-					<div class="col-md-2 col-sm-4">
-						<div class="footer-widget">
-							<h3 class="footer-title">OUR SERVICE</h3>
-							<ul class="block-content">
-								<li><a href="#">My orders</a></li>
-								<li><a href="#">My credit slips</a></li>
-								<li><a href="#">Sitemap</a></li>
-								<li><a href="#">My addresses</a></li>
-								<li><a href="#">My personal info</a></li>
-							</ul>
-						</div>
+					<div class="col-md-1 col-sm-4">
+						&nbsp;
 					</div>
-					<div class="col-md-3 col-sm-4">
+					<div class="col-md-4 col-sm-4">
 						<div class="footer-widget">
-							<h3 class="footer-title">OPENING TIME</h3>
+							<h3 class="footer-title" style="margin-bottom:15px">Tetap Terhubung Dengan Kami</h3>
+							<div class="footer-time" style="font-size:20px">
+								<a href="#"><span class="fa fa-facebook" style="color:#ffffff;margin-right:15px"></span></a>
+								<a href="#"><span class="fa fa-instagram" style="color:#ffffff;margin-right:15px"></span></a>
+								<a href="#"><span class="fa fa-twitter" style="color:#ffffff;margin-right:15px"></span></a>
+								<a href="#"><span class="fa fa-whatsapp" style="color:#ffffff;margin-right:15px"></span></a>
+							</div>
+
+							<h3 class="footer-title" style="margin-top:40px;margin-bottom:5px">Dapatkan Aplikasi Kami</h3>
 							<div class="footer-time">
-								<p><span class="ft-content"><span class="day">Monday - Friday</span><span class="time">9:00 - 22:00</span></span></p>
-								<p><span class="ft-content"><span class="day">Saturday</span><span class="time">10:00 - 24:00</span></span></p>
-								<p><span class="ft-content"><span class="day">Sunday</span><span class="time">12:00 - 24:00</span></span></p>
-								<p><span class="ft-content"><span class="day">Thursday</span><span class="time">Free Shipping</span></span></p>
-								<p><span class="ft-content"><span class="day">Friday</span><span class="time">sale of 30%</span></span></p>
+								<p><span class="ft-content" style="font-size:12px">Download Aplikasi KangKoding Sekarang Juga!</span></p>
+								<p>
+									<span class="ft-content" style="display:inline-block">
+										<a href="#"><img src="/img/icon/download_google_play.svg" style="width:150px;margin-right:10px"></a>
+										<a href="#"><img src="/img/icon/download_app_store.svg" style="width:150px"></a>
+									</span>
+								</p>
 							</div>
 						</div>
 					</div>
